43d459304ccfe70ee8886da4295737d16b53f98d
fauzandp <fauzan27dp@gmail.com>
2023-05-27 10:09:50 +0700

    Project28-30 | Memory Pairs Game setup UI using storyboard

---
+++ Project28-30_self/Controller/Card.swift
@@ -0,0 +1,10 @@
+//
+//  Card.swift
+//  Project28-30_self
+//
+//  Created by Fauzan Dwi Prasetyo on 27/05/23.
+//
+
+import Foundation
+
+
--- Project28-30_self/Controller/GameViewController.swift
+++ Project28-30_self/Controller/GameViewController.swift
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GameViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Match Pairs"
     }
 
 
